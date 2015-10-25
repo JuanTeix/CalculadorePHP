771f367e6604ce1ac88f46f8d20909101e92b87a
Juan Teixeira <juanteix89@gmail.com>
2015-10-25 10:45:38 -0430

    aplicando CSS basico

--- diseno.php
+++ diseno.php
@@ -3,22 +3,25 @@
 <head>
 	<title>Calculadora</title>
 	<meta charset="utf-8" />
+	<link rel="stylesheet" href="dis.css">
 </head>
 <body>
    
-   <form action="calculadora.php" method="post" name="formulario">
+   <form  class="form1" action="calculadora.php" method="post" name="formulario">
 
-        <h2>Selecciona la Operacòn matematica</h2>
-           <input type="text" name="C1">
-           <input type="text" name="C2">
-           <input type="text" name="C3">
+        <h2 class="h2_titulo">Selecciona la Operacòn matematica</h2>
+          <div class="div1"> 
+          <input class="input1" type="text" name="C1"><br/><br/>
+           <input class="input1" type="text" name="C2"><br/><br/>
+           <input class="input1" type="text" name="C3"><br/>
+          </div>
         <h2>Coloque los Valores</h2>
         <select name="lista">
            <option value="Sumar">Sumar</option>
            <option value="Restar">Restar</option>
            <option value="Multiplicar">Multiplicar</option>
            <option value="Dividir">Dividir</option>
-        </select>  
+        </select> <br/><br/>
           <input type="submit" value="Ver Resultado"> 
    	
     </form>
