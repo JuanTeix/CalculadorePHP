6746a818e33e30afa1307961bfb53fbe5857ea21
Juan Teixeira <juanteix89@gmail.com>
2015-10-25 09:05:48 -0430

    Cree los archivos diseño.php y calculadora.php

---
+++ diseño.php
@@ -0,0 +1,28 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+   
+   <form action="calculadora.php" method="post" name="formulario">
+
+        <h2>Selecciona la Operacòn matematica</h2>
+           <input type="text" name="C1">
+           <input type="text" name="C2">
+           <input type="text" name="C3">
+        <h2>Coloque los Valores</h2>
+        <select>
+           <option value="Sumar">Sumar</option>
+           <option value="Restar">Restar</option>
+           <option value="Multiplicar">Multiplicar</option>
+           <option value="Dividir">Dividir</option>
+        </select>   
+   	
+   </form>
+
+</body>
+
+</html>
